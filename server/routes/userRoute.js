b486c0cece5a92ec7e4f6570c8dc629dd7a15e65
Rohit Hegade <60090023+Rohithegade02@users.noreply.github.com>
2024-05-29 15:28:24 +0530

    getprofile removed

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,13 +1,10 @@
 import express from 'express';
-import { isAuthenticated } from '../middleware/auth.js';
-import { getProfile, loginUser, logout, registerUser } from '../controller/userController.js';
+import { loginUser,  registerUser } from '../controller/userController.js';
 
 
 const router = express.Router()
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
-router.get('/getProfile', isAuthenticated,getProfile)
-router.get('/logout', logout)
 
 export default router;
